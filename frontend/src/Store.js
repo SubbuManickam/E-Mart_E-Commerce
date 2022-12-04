aab8310cb17335514c39623d3ddd074d7ed7e6fb
Subbu <subbu.manickam@ghd.com>
2022-12-04 16:38:04 -0500

    Sign In Screen after from Cart

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -4,6 +4,7 @@ export const Store = createContext();
 
 
 const initialState = {
+    userInfo: localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null,
     cart: {
         cartItems: localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [],
     },
@@ -47,6 +48,10 @@ function reducer(state, action) {
                 }
             };
         }
+
+        case 'USER_SIGNIN': return { ...state, userInfo: action.payload };
+
+        case 'USER_SIGNOUT': return { ...state, userInfo: null };
         default: return state;
     }
 }
