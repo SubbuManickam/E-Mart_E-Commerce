07636af353f910fbbfaab3865e9e287c97dd5a4b
Ajit Patil <ajitpatil@Ajits-MacBook-Pro.local>
2022-12-07 10:41:04 -0500

    backend api's-Orderscreen,history and Userprofile

---
+++ server.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import data from './data.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import seedRouter from './routes/seedRoutes.js';
+import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
+import orderRouter from './routes/orderRoutes.js';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    console.log("Connected to DB")
+}).catch((err) => {
+    console.log(err.message);
+});
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/api/keys/paypal', (req, res) => {
+    res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+  });
+
+app.use('/api/seed', seedRouter);
+
+app.use('/api/products', productRouter);
+
+app.use('/api/users', userRouter);
+
+app.use('/api/orders', orderRouter);
+
+app.use((err, req, res, next) => {
+    res.status(500).send({message: err.message});
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(`App started at http://localhost:${port}`);
+})
