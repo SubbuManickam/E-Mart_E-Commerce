93528c4467688d208395ced41614d8af99159a43
Ajit Patil <ajitpatil@Ajits-MacBook-Pro.local>
2022-12-07 16:38:48 -0500

    Admin UserManagement Userlistscreen and UserEdit

--- src/App.js
+++ src/App.js
@@ -27,6 +27,8 @@ import DashboardScreen from './screens/DashBoardScreen';
 import ProductListScreen from './screens/ProductListScreen';
 import ProductEditScreen from './screens/ProductEditScreen';
 import OrderListScreen from './screens/OrderListScreen';
+import UserListScreen from './screens/UserListScreen';
+import UserEditScreen from './screens/UserEditScreen';
 
 
 function App() {
@@ -150,6 +152,14 @@ function App() {
                 }
               ></Route>
                <Route
+                path="/admin/users"
+                element={
+                  <AdminRoute>
+                    <UserListScreen />
+                  </AdminRoute>
+                }
+              ></Route>
+               <Route
                 path="/admin/products"
                 element={
                   <AdminRoute>
@@ -162,6 +172,14 @@ function App() {
                 element={
                   <AdminRoute>
                     <ProductEditScreen />
+                  </AdminRoute>
+                }
+              ></Route>
+                <Route
+                path="/admin/user/:id"
+                element={
+                  <AdminRoute>
+                    <UserEditScreen />
                   </AdminRoute>
                 }
               ></Route>
